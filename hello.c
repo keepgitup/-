ddaa7b338feeef1c80273681ea0f511676bc4337
keep <flymayday112@gmail.com>
2024-02-01 13:13:15 +0800

    enter the positive number then print

--- hello.c
+++ hello.c
@@ -5,7 +5,17 @@ int main(){
     printf("Enter an integer:");
     scanf("%d",&number);
 
-    
+    if(number>0){
+        printf("You entered a positive number.\n");
+    }
+    else if (number < 0)
+    {
+        printf("You entered a negative number.\n");
+    }
+    else
+    {
+        printf("You entered zero.\n");
+    }
 
-
+    return 0;
 }
